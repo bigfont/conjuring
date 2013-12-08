6db771a522d0d5a8e08bea512ff0a4123ada80c8
Shaun Luttin <shaunluttin@gmail.com>
2013-12-08 15:59:33 -0800

    Start a WebApi and an AngularJS app.

---
+++ MyConjuringWebApp/app.js
@@ -0,0 +1,9 @@
+﻿var myApp = angular.module('conjuringApp', []);
+
+
+myApp.controller('BookCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    var books;
+    books = $http.get('http://localhost:2188/api/book/get');
+
+}]);
