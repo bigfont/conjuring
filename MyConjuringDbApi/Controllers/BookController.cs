d93d33c1180b542187eb45d9f20ce2d193510643
Shaun Luttin <shaunluttin@gmail.com>
2013-12-08 15:59:33 -0800

    Start a WebApi and an AngularJS app.

---
+++ MyConjuringDbApi/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using MyConjuringDbApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyConjuringDbApi.Controllers
+{
+    public class BookController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Book> Get()
+        {
+            ConjuringDb db = new ConjuringDb();
+            return db.Books;
+        }
+    }
+}
