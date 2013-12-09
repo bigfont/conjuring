46ed30efec706b2d3d55c9a954b47a3d28c20729
Shaun Luttin <shaunluttin@gmail.com>
2013-12-08 16:38:05 -0800

    Add an http get.

--- MyConjuringDbApi/app.js
+++ MyConjuringDbApi/app.js
@@ -2,8 +2,8 @@
 
 
 myApp.controller('BookCtrl', ['$scope', '$http', function ($scope, $http) {
-
-    var books;
-    books = $http.get('http://conjuring.azurewebsites.net/api/book/get');
+    
+    var books = $http.get('http://conjuring.azurewebsites.net/api/book/get');
+    $scope.books = books;
 
 }]);
