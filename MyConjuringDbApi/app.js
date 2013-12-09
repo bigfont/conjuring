83ffca54887027821b563190dcb699dbeb3739ff
Shaun Luttin <shaunluttin@gmail.com>
2013-12-08 16:59:35 -0800

    Add subtitle.

--- MyConjuringDbApi/app.js
+++ MyConjuringDbApi/app.js
@@ -1,6 +1,5 @@
 ﻿var myApp = angular.module('conjuringApp', []);
 
-
 myApp.controller('BookCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
     $http.get('http://conjuring.azurewebsites.net/api/book/get')
         .success(function (data, status, headers, config) {
