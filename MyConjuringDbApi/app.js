9bd811d42c374dc3a8bf298ca9a040df7d6ada2b
Shaun Luttin <shaunluttin@gmail.com>
2013-12-08 16:34:04 -0800

    Adjust the web service url.

--- MyConjuringDbApi/app.js
+++ MyConjuringDbApi/app.js
@@ -4,6 +4,6 @@
 myApp.controller('BookCtrl', ['$scope', '$http', function ($scope, $http) {
 
     var books;
-    books = $http.get('http://localhost:2188/api/book/get');
+    books = $http.get('http://conjuring.azurewebsites.net/api/book/get');
 
 }]);
