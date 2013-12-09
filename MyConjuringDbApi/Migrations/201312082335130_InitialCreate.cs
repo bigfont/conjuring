c7d9f06d32df6d8e88f9309c00aee0cdbf8ae396
Shaun Luttin <shaunluttin@gmail.com>
2013-12-08 17:16:38 -0800

    Add a database migration.

---
+++ MyConjuringDbApi/Migrations/201312082335130_InitialCreate.cs
@@ -0,0 +1,26 @@
+namespace MyConjuringDbApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Books");
+        }
+    }
+}
