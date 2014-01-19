d3a7ba42a9808d6d984e6d3ce94cab259970f221
Shaun Luttin <shaunluttin@gmail.com>
2014-01-19 14:55:08 -0800

    Add a comment about minification.

--- ConjuringClub/App_Start/BundleConfig.cs
+++ ConjuringClub/App_Start/BundleConfig.cs
@@ -4,19 +4,23 @@ using System.Web.Optimization;
 namespace ConjuringClub
 {
     public class BundleConfig
-    {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+    {         
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.UseCdn = true;
-
+            // 
+            // Files will be minified and bundled iff EnableOptimizations is true OR compilation.debug is false.
+            // EnableOptimizations overrides compilation.debug attribute.
+            // 
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;                
+            
             const string angularCdn = "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.4/angular.js";
             bundles.Add(new ScriptBundle("~/cdn/angular", angularCdn).Include(
                 "~/Scripts/angular.js"));
-
+            
             bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
                 "~/Scripts/app.js"));
-
+            
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/app.css"));
