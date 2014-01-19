84112b27949dcac022958296e3e0fb8ddd66d27a
Shaun Luttin <shaunluttin@gmail.com>
2014-01-19 14:47:46 -0800

    Add the css and javascript through bundling.

--- ConjuringClub/App_Start/BundleConfig.cs
+++ ConjuringClub/App_Start/BundleConfig.cs
@@ -8,21 +8,18 @@ namespace ConjuringClub
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.UseCdn = true;
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            const string angularCdn = "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.4/angular.js";
+            bundles.Add(new ScriptBundle("~/cdn/angular", angularCdn).Include(
+                "~/Scripts/angular.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
+                "~/Scripts/app.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/app.css"));
         }
     }
 }
---
+++ ConjuringClub/Scripts/app.js
@@ -0,0 +1,107 @@
+﻿//app
+var myApp = angular.module('conjuringApp', []);
+
+//constants
+myApp.constant('webApiConst', '/api/');
+
+//controllers
+myApp.controller('BookCtrl', ['$scope', '$http', '$window', 'webApiConst', function ($scope, $http, $window, webApiConst) {
+
+    $scope.bookSearchComparitor = bookSearchComparitor;
+
+    $http.get(webApiConst + 'book/get').
+        success(function (data, status, headers, config) {
+            $scope.books = data;
+            $scope.search = createBookSearchObj($scope.books[0]);
+        }).
+        error(function (data, status, headers, config) {
+            //Use this data in case of db error
+            data = [
+                { "Title": "Magic Of The World", "FirstName": "John", "LastName": "Mulholland", "Publisher": "Charles Scribner's Sons", "PublishYear": 1965, "PublishLocation": "New York: NY" },
+                { "Title": "The Magic Of Micheal Ammar", "FirstName": "Michael", "LastName": "Ammar", "Publisher": "L&L Publishing ", "PublishYear": 1991, "PublishLocation": "Tahoma: CA" },
+                { "Title": "Impossibilia", "FirstName": "John", "LastName": "Bannon", "Publisher": "L&L Publishing ", "PublishYear": 1990, "PublishLocation": "Tahoma: CA" },
+                { "Title": "The Blackstone Book Of Magic And Illusion", "FirstName": "Harry", "LastName": "Blackstone", "Publisher": "New Market Press", "PublishYear": 1985, "PublishLocation": "New York: NY" },
+                { "Title": "Modern Coin Magic", "FirstName": "J. B. ", "LastName": "Bobo", "Publisher": "Magic Inc", "PublishYear": 1988, "PublishLocation": "Chicago: IL" },
+                { "Title": "Modern Coin Magic", "FirstName": "J. B. ", "LastName": "Bobo", "Publisher": "Dover Publications", "PublishYear": 1982, "PublishLocation": "New York: NY" },
+                { "Title": "The Royal Road To Card Magic", "FirstName": "Frederick", "LastName": "Braue", "Publisher": "Faber & Faber", "PublishYear": 1975, "PublishLocation": "London: England" },
+                { "Title": "Expert Card Technique", "FirstName": "Frederick", "LastName": "Braue", "Publisher": "Dover Publications", "PublishYear": 1974, "PublishLocation": "New York: NY" }];
+            $scope.books = data;
+            $scope.search = createBookSearchObj($scope.books[0]);
+        });
+}]);
+
+myApp.controller('ConductCtrl', ['$scope', function ($scope) {
+
+    $scope.aspirations = [
+        {
+            Notion: 'Arrive on time',
+            Example: 'If the club starts at 3:30 pm, we intend arrive within 5 minutes of 3:30 pm.'
+        },
+        {
+            Notion: 'Finish on time',
+            Example: 'If the club ends at 5 pm, we intend to be out by 5 pm.'
+        },
+        {
+            Notion: 'Be supportive',
+            Example: 'If we know how something works, we intend to say, "You performed that well" not "I know how you did that."'
+        },
+        {
+            Notion: 'Speak cleanly',
+            Example: 'If we are frustrated, we intend to say, "Wow, this is hard" not "@#$^%, this is hard."'
+        },
+
+    ];
+
+}]);
+
+myApp.controller('LevelCtrl', ['$scope', function ($scope) {
+
+    $scope.criteria = [
+        { Skill: 'Dealing', Standard: 'Deal 52 cards, one at a time, from a dealers grip in one minute.' },
+        { Skill: 'Shuffling', Standard: 'Run 52 cards, one at a time, from an overhand shuffle grip in one minute.' },
+        { Skill: 'Spreading', Standard: 'Spread the cards from the left to the right hand.' },
+        { Skill: 'Cutting', Standard: 'Cut the cards in the hands from dealers grip using the specified technique.' }
+    ];
+
+}]);
+
+
+
+function createBookSearchObj(bookObj) {
+    var searchObj;
+
+    searchObj = {};
+    for (var key in bookObj) {
+        if (bookObj.hasOwnProperty(key)) {
+            searchObj[key] = "";
+        }
+    }
+}
+
+function bookSearchComparitor(expected, actual) {
+
+    var containsAll;
+    var actualSplit;
+
+    if (actual.length === 0) {
+        return true;
+    }
+
+    actual = makeString(actual).toLowerCase();
+    expected = makeString(expected).toLowerCase();
+
+    actualSplit = actual.split(" ");
+    for (var i = 0; i < actualSplit.length; ++i) {
+
+        containsAll = expected.indexOf(actualSplit[i]) !== -1;
+
+    }
+    return containsAll;
+};
+
+function makeString(input) {
+    if (typeof (input) !== 'string') {
+        input = '' + input;
+    }
+    return input;
+}
