e37ac885de0885dc3627e0bb259f521f3e665adc
Shaun Luttin <shaunluttin@gmail.com>
2014-01-19 14:22:04 -0800

    Add a new ASP.NET Web Application, with Web API, and with Individual User Accounts.

---
+++ ConjuringClub/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ConjuringClub.Startup))]
+
+namespace ConjuringClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
