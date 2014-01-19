909d740403edb720016d8e068b9528bc2284ce45
Shaun Luttin <shaunluttin@gmail.com>
2014-01-19 14:22:04 -0800

    Add a new ASP.NET Web Application, with Web API, and with Individual User Accounts.

---
+++ ConjuringClub/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace ConjuringClub.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
