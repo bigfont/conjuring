dd768ab6ec683049040f74a5908e5622ec92fbef
Shaun Luttin <shaunluttin@gmail.com>
2014-01-19 14:59:36 -0800

    Add the BookController and related models.

---
+++ ConjuringClub/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyConjuringDbApi.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public int PublishYear { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public int AuthorID { get; set; }
+        public int PublisherID { get; set; }
+    }
+}
