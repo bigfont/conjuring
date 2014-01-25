ec0b22abbe898a93625ab136d7382840d779b62b
Shaun Luttin <shaunluttin@gmail.com>
2014-01-24 19:32:02 -0800

    Use the IdentityDbContext instead of the DbContext.

--- ConjuringClub/Models/ConjuringDb.cs
+++ ConjuringClub/Models/ConjuringDb.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MyConjuringDbApi.Models
 {
-    public class ConjuringDb : DbContext
+    public class ConjuringDb : IdentityDbContext<IdentityUser>
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Authors> Authors { get; set; }
