08d84203452615ec45668c7301ca240e611fabf1
Shaun Luttin <shaunluttin@gmail.com>
2014-05-01 12:36:31 -0700

    Embed a OneDrive document.

---
+++ ConjuringClub/ViewModels/HFrameViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConjuringClub.ViewModels
+{
+    public class HFrameViewModel
+    {
+        public string Src { get; set; }
+        public string FigCaption { get; set; }
+
+    }
+}
