a743b225dc311a7c5f268ac198e6a26bd54fcc7a
Shaun Luttin <shaunluttin@gmail.com>
2014-04-30 18:29:14 -0700

    Turn off debug.

---
+++ ConjuringClub/ViewModels/AsideImageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConjuringClub.ViewModels
+{
+    public class AsideImageViewModel
+    {
+        public string Src { get; set; }
+        public string FigCaption { get; set; }
+        public AsideImageViewModel(string src, string figCaption)
+        {
+            this.Src = src;
+            this.FigCaption = figCaption;
+        }
+    }
+}
