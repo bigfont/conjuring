6e24dda99b75986eb1b103fc64cff8cea9f33a2a
Shaun Luttin <shaunluttin@gmail.com>
2014-05-14 12:17:06 -0700

    Add subnav.

--- ConjuringClub/App_Start/BundleConfig.cs
+++ ConjuringClub/App_Start/BundleConfig.cs
@@ -12,22 +12,14 @@ namespace ConjuringClub
             // EnableOptimizations overrides compilation.debug attribute.
             // 
             BundleTable.EnableOptimizations = false;
-            bundles.UseCdn = true;                
-            
-            const string angularCdn = "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.4/angular.js";
-            bundles.Add(new ScriptBundle("~/cdn/angular", angularCdn).Include(
-                "~/Scripts/angular/angular.js"));
             
             bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
                 "~/Scripts/google-analytics-object.js",
                 "~/Scripts/controllers/*.js",
+                "~/Scripts/bigfont-toc.js",
                 "~/Scripts/app.js"));
 
-            const string bootstrapCdn = "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css";
-            bundles.Add(new StyleBundle("~/bundles/bootstrap", bootstrapCdn).Include(
-                "~/Content/bootstrap.css"));
-
-            bundles.Add(new StyleBundle("~/bundles/app").Include("~/Content/app.css"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/app.css"));
         }
     }
 }
---
+++ ConjuringClub/Scripts/bigfont-toc.js
@@ -0,0 +1,79 @@
+﻿(function () {
+
+    'use strict';
+    /*global $:false, window:false, document:false */
+    /*jslint white: true */
+
+    // 
+    // Usuage - the following usuage uses the Bootstrap 3.0.3 Collapse as the trigger.
+    // --------------------------------------------------
+    //
+    //
+    // <div id="subnav">
+    //     <ul></ul>
+    // </div>
+    //
+    //
+    // <section>
+    //     <header>
+    //         <h1 id="about">About Us</h1>
+    //     </header>
+    // </section>
+    //
+    // <section >
+    //     <h1 id="about">About Us</h1>
+    // </section>
+    //
+
+    function createToc(ulSelector, iconClass) {
+
+        var sectionId, sectionTitle, h, ul, li, a, i;
+
+        // get the ul
+        ul = $(ulSelector);
+
+        // iterate the sections
+        $('section').not('.toc-hidden').each(function () {
+
+            // get the id and title of the h1, h2, h3
+
+            h = $(this)
+                .find('h1', 'h2', 'h3', 'h4')
+                .first();
+
+            sectionId = h.attr('id');                        
+
+            if (h[0] != null)
+            {
+                // strip out child element text from the title e.g. <small/> and <span/>
+                sectionTitle = h[0]
+                    .firstChild
+                    .textContent;
+
+                // create the li, a, and i
+                li = $('<li/>');
+
+                a = $('<a />', {
+                    href: '#' + sectionId,
+                    text: sectionTitle
+                });
+
+                i = $('<i/>', {
+                    'class': iconClass
+                });
+
+                // mush them together and append to the ul
+                i.prependTo(a);
+                a.appendTo(li);
+                li.appendTo(ul);
+            }
+        });
+    }
+
+    $(document).ready(function () {
+
+        createToc('#subnav > ul', 'icon-chevron-right');
+
+    });
+
+}());
