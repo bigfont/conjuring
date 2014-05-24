04a86baa2afb3948139c61012a7294efa642e97f
Shaun Luttin <shaunluttin@gmail.com>
2014-05-23 18:37:07 -0700

    Add members.

---
+++ ConjuringClub/ViewModels/MemberViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConjuringClub.ViewModels
+{
+    public class MemberViewModel
+    {
+        public string HeadshotSrc { get; set; }
+        public string About { get; set; }
+        public string FirstName { get; set; }
+
+        public MemberViewModel(string headshotSrc, string firstName, string about)
+        {
+            this.HeadshotSrc = headshotSrc;
+            this.FirstName = firstName;
+            this.About = about;
+        }
+    }
+}
