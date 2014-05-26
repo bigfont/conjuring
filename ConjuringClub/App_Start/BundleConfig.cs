3de532f0ca86d4a89ea0a91609326b6e9d88597b
Shaun Luttin <shaunluttin@gmail.com>
2014-05-26 12:42:37 -0700

    Add ie-10 css and js.

--- ConjuringClub/App_Start/BundleConfig.cs
+++ ConjuringClub/App_Start/BundleConfig.cs
@@ -11,15 +11,18 @@ namespace ConjuringClub
             // Files will be minified and bundled iff EnableOptimizations is true OR compilation.debug is false.
             // EnableOptimizations overrides compilation.debug attribute.
             // 
-            BundleTable.EnableOptimizations = false;
+            BundleTable.EnableOptimizations = true;
             
             bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
                 "~/Scripts/google-analytics-object.js",
                 "~/Scripts/controllers/*.js",
                 "~/Scripts/bigfont-toc.js",
+                "~/Scripts/IE-10.js",
                 "~/Scripts/app.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/app.css"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/app.css",
+                "~/Content/IE-10.css"));
         }
     }
 }
