3d19b0bdd86988d804e75ab495baf5ed67a18d1e
Shaun Luttin <shaun@bigfont.ca>
2014-08-29 10:16:24 -0700

    Add a conditional to membership config.

--- ConjuringClub/App_Start/MembershipConfig.cs
+++ ConjuringClub/App_Start/MembershipConfig.cs
@@ -15,27 +15,30 @@ namespace ConjuringClub
         public static void CreateLoginsAndRoles(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             string path = HttpContext.Current.Server.MapPath(@"~/Private/admin-login.json");
-            string json = File.ReadAllText(path);
-            JObject o = JObject.Parse(json);
-
-            string username = (string)o["username"];
-            string password = (string)o["password"];
-            string role = "admin";
-
-            if (!roleManager.RoleExists(role))
+            if (File.Exists(path))
             {
-                roleManager.Create(new IdentityRole(role));
-            }
+                string json = File.ReadAllText(path);
+                JObject o = JObject.Parse(json);
 
-            if (userManager.FindByName(username) == null)
-            {
-                IdentityUser user = new IdentityUser(username);
-                IdentityResult result = userManager.Create(user, password);
-                if(result.Succeeded && !userManager.IsInRole(user.Id, role))
+                string username = (string)o["username"];
+                string password = (string)o["password"];
+                string role = "admin";
+
+                if (!roleManager.RoleExists(role))
                 {
-                    userManager.AddToRole(user.Id, role);
+                    roleManager.Create(new IdentityRole(role));
                 }
-            }            
+
+                if (userManager.FindByName(username) == null)
+                {
+                    IdentityUser user = new IdentityUser(username);
+                    IdentityResult result = userManager.Create(user, password);
+                    if (result.Succeeded && !userManager.IsInRole(user.Id, role))
+                    {
+                        userManager.AddToRole(user.Id, role);
+                    }
+                }
+            }
         }
     }
 }
