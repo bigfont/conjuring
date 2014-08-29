5531a8958a0265b225b0079e6196a4e6a3b9cde7
Shaun Luttin <shaun@bigfont.ca>
2014-08-29 10:52:39 -0700

    Start on the .cs youtube api.

---
+++ ConjuringClub/Controllers/YouTubeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ConjuringClub.ViewModels;
+
+namespace ConjuringClub.Controllers
+{
+    public class YouTubeController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<YouTubeVideoViewModel> Get()
+        {
+            List<YouTubeVideoViewModel> videos = new List<YouTubeVideoViewModel>();
+            return videos;
+        }
+    }
+}
