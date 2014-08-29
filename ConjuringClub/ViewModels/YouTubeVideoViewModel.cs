0d31c020bf2cdd90df39f84e5cb1ae40106a27ab
Shaun Luttin <shaun@bigfont.ca>
2014-08-29 10:28:38 -0700

    Add a YouTubeVideoViewModel.

---
+++ ConjuringClub/ViewModels/YouTubeVideoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConjuringClub.ViewModels
+{
+    public class YouTubeVideoViewModel
+    {
+        public YouTubeVideoViewModel(string videoId, string videoTitle)
+        {
+            this.VideoId = videoId;
+            this.VideoTitle = videoTitle;
+        }
+        public string VideoId { get; set; }
+        public string VideoTitle { get; set; }
+    }
+}
