7e0bb4a141311cba0077d97ddea45e9437603fbf
Shaun Luttin <shaun@bigfont.ca>
2014-10-18 13:50:09 -0700

    Add two photo galleries.

---
+++ ConjuringClub/ViewModels/ImageGalleryFromFolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConjuringClub.ViewModels
+{
+    public class ImageGalleryFromFolderViewModel
+    {
+        public string GalleryDirectory { get; set; }
+    }
+}
