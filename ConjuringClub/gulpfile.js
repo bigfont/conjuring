8efce6e4eb2abbb35803fc8521bbfd13c6ac75a2
Shaun Luttin <shaunluttin@gmail.com>
2015-06-08 12:56:12 -0700

    Compile less with gulp.

--- ConjuringClub/gulpfile.js
+++ ConjuringClub/gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('gulp-rename');
+var rename = require('gulp-rename');
 
 gulp.task('default', function () {
 
 
+
+});
+
+gulp.task('less', function () {
+
+    return gulp.src('./Content/app.less')
+        .pipe(less())        
+        .pipe(gulp.dest('./Content/'));
 
 });
